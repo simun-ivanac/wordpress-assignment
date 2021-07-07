e7edf4fabb477472118adb98bb39e711c9e91fe1
Simun Ivanac <ivanac.simun@gmail.com>
2021-07-07 18:13:30 +0200

    Fix function typo

--- src/Blocks/custom/latest-posts/components/latest-posts-options.js
+++ src/Blocks/custom/latest-posts/components/latest-posts-options.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { PanelBody } from '@wordpress/components';
 
-export const ExampleOptions = () => {
+export const LatestPostsOptions = () => {
 	return (
 		<PanelBody title={__('Latest Post', 'Assignment')}>
 		</PanelBody>
