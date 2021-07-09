d8fbe5ff7f95f9aef98d52b7433d77a3332e46a8
Simun Ivanac <ivanac.simun@gmail.com>
2021-07-09 13:35:16 +0200

    Remove unused block options function

--- src/Blocks/custom/latest-posts/latest-posts-block.js
+++ src/Blocks/custom/latest-posts/latest-posts-block.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { InspectorControls } from '@wordpress/block-editor';
 import { Fragment } from '@wordpress/element';
 import { LatestPostsEditor } from './components/latest-posts-editor';
-import { LatestPostsOptions } from './components/latest-posts-options';
 
 export const LatestPosts = (props) => {
 	return (
 		<Fragment>
-			<InspectorControls>
-				<LatestPostsOptions {...props} />
-			</InspectorControls>
 			<LatestPostsEditor {...props} />
 		</Fragment>
 	);
