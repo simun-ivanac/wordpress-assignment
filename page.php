0693f9d45ed33f1afa2bfe2213449a9eb40b44ef
Simun Ivanac <ivanac.simun@gmail.com>
2021-07-14 13:34:24 +0200

    Customize and display main heading

--- page.php
+++ page.php
@@ -11,7 +11,15 @@ get_header();
 if (have_posts()) {
 	while (have_posts()) {
 		the_post();
-		the_title();
+		echo Components::render( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			'heading',
+			[
+				'headingContent' => the_title('', '', false),
+				'headingLevel' => 1,
+				'headingAlign' => 'center',
+				'headingSize' => 'superbig',
+			]
+		);
 		the_content();
 	}
 }
